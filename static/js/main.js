5d3667e81362031b7290af317714f5a3e22b0df9
Abhirup Karmakar <abhirupkarmakar@Abhirups-MacBook-Pro.local>
2026-09-16 19:09:20 +0530

    Fix quantity stepper decrement bug (Fixes #88)

--- static/js/main.js
+++ static/js/main.js
@@ -114,7 +114,7 @@ function setupQuantityStepper() {
     });
 
     decrease.addEventListener("click", function () {
-        input.value = parseInt(input.value) + 1;
+        input.value = parseInt(input.value) - 1;
     });
 }
 
